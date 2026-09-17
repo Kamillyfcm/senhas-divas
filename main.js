5fdb8efe31027cb010d42d374a5220a21accbefa
Kamillyfcm <kamilly.menin@escola.pr.gov.br>
2026-09-17 19:05:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,22 @@ const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelectorAll('.parametro-senha__botao')
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-botoes[0].onclick = diminuitamanho;
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
-function diminuitamanho(){
-    tamanhoSenha = tamanhoSenha-1;
-    numeroSenha.textContent= tamanhoSenha
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha-1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        tamanhoSenha = tamanhoSenha+1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
 console.log(botoes);
